25f886eb9c9150dc57bd5bb7357fafde1d7e1958
gehlisboa <gehmoraeslisboa@gmail.com>
2025-03-13 18:23:53 -0300

    7 maravilhas

--- 7_maravilhas/china.php
+++ 7_maravilhas/china.php
@@ -4,6 +4,33 @@
         <div class="col-12 d-flex justify-content-center align-items-center">
             <h1>Muralha da China</h1>
         </div>
+    <div>
+
+        <div class="row">
+            <div class="cow-3">
+                <div class="card">
+                    <img style = widght 100% src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqrnBKipdqtLgmrznqbEGC5cSGQ24IH24flA&s" alt="">
+                </div>
+            </div>
+
+            <div class="cow-3">
+                <div class="card">
+                    <img style = widght 100% src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqrnBKipdqtLgmrznqbEGC5cSGQ24IH24flA&s" alt="">
+                </div>
+            </div>
+
+            <div class="cow-3">
+                <div class="card">
+                    <img style = widght 100% src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqrnBKipdqtLgmrznqbEGC5cSGQ24IH24flA&s" alt="">
+                </div>
+            </div>
+
+            <div class="cow-3">
+                <div class="card">
+                    <img style = widght 100% src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqrnBKipdqtLgmrznqbEGC5cSGQ24IH24flA&s" alt="">
+                </div>
+            </div>
+            
+        </div>
     </div>
-</div>
 <?php include './footer.php';?>
