8a9f63708915edcea56ea4fd081af18b20890008
gehlisboa <gehmoraeslisboa@gmail.com>
2025-03-13 18:05:54 -0300

    ex

--- 7_maravilhas/china.php
+++ 7_maravilhas/china.php
@@ -1,4 +1,9 @@
 <?php include './header.php';?>
-
-<h1>China</h1>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center align-items-center">
+            <h1>Muralha da China</h1>
+        </div>
+    </div>
+</div>
 <?php include './footer.php';?>
