5cc8661493b0ed7727ab2c1feacc31e6b8045309
gehlisboa <gehmoraeslisboa@gmail.com>
2025-04-09 21:53:27 -0300

    php

--- exercicios_entregar/ex3.php
+++ exercicios_entregar/ex3.php
@@ -1,16 +1,50 @@
 <?php
-  $etec = [
-    'curso' => [
-        'curso' => 'Desenvolvimento de Sistemas',
-    ],
-    'materia' => [
-        'materia 1' => 'programção mobile'
-        'materia 2' => 'programação web'
-        'materia 3' => 'tecnicas avançadas de programação de algoritmos'
-        'materia 4' => 'ingles'
-    ],
-    'modulo' => [
-    
+
+$etec = [
+    'cursos' => [
+        'Desenvolvimento de Sistemas' => [
+            'Programação Mobile' => [
+                'Módulo I',
+                'Módulo II',
+            ],
+            'Programação Web' => [
+                'Módulo I',
+                'Módulo II',
+                'Módulo III',
+                'Módulo IV',
+            ],
+            'Técnicas Avançadas de Programação de Algoritmos' => [],
+            'Inglês' => [
+                'Módulo I',
+                'Módulo II',
+                'Módulo III',
+            ],
+        ],
+        'Administração' => [
+            'Recursos Humanos' => [
+                'Módulo I',
+                'Módulo II',
+            ],
+            'Gestão de Pessoas' => [],
+            'Teoria da Administração' => [],
+            'Inglês' => [
+                'Módulo I',
+            ],
+        ]
     ]
 ];
+
+echo "- Etec<br>";
+
+foreach ($etec['cursos'] as $curso => $disciplinas) {
+    echo "&emsp;- $curso<br>";
+    foreach ($disciplinas as $disciplina => $modulos) {
+        echo "&emsp;&emsp;- $disciplina<br>";
+        if (!empty($modulos)) {
+            foreach ($modulos as $modulo) {
+                echo "&emsp;&emsp;&emsp;- $modulo<br>";
+            }
+        }
+    }
+}
 ?>
