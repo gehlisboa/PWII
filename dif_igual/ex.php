edb7fd65e134b3cbfbafef61cc1fb2cbd295d5ff
gehlisboa <gehmoraeslisboa@gmail.com>
2025-04-10 13:47:26 -0300

    ex

---
+++ dif_igual/ex.php
@@ -0,0 +1,15 @@
+<?php
+$num=[0,7,7,8,9,87,7,6,,6,89,8,]
+$igual0=0;
+$diferente=0;
+ foreach ($num as $key => $value) {
+    if($value==0){
+        $igual0++
+    }
+    else{
+        $diferente++
+    }
+ }
+ echo "o jdhfnhdgsft".$igual0."uesryserfbksug"
+
+?>
